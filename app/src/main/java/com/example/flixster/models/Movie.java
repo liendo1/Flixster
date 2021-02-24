f3578e814ca67ce60d9d6fb5410448a56cbee8c6
liendo1 <panaliendo@outlook.com>
2021-02-24 14:05:43 +0100

    Color change

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,16 +3,26 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
+@Parcel
 public class Movie {
+
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
     String overView;
+    double rating;
+
+    //empty constructer needed for parcel library
+    public Movie(){}
 
     //if there is no key called poster or title or overview gets an jsonException
     public Movie(JSONObject jsonObject) throws JSONException {
@@ -20,7 +30,8 @@ public class Movie {
         backdropPath = jsonObject.getString("backdrop_path");
         title = jsonObject.getString("title");
         overView = jsonObject.getString("overview");
-
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
     public static List<Movie>fromJsonArray(JSONArray movieJsonArray) throws JSONException {
         List<Movie>movies = new ArrayList<>();
@@ -46,4 +57,11 @@ public class Movie {
     public String getOverView() {
         return overView;
     }
+
+    public double getRating(){return rating;}
+
+    public int getMovieId() {
+        return movieId;
+    }
 }
+
